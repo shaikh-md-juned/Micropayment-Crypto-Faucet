582f667917a2be2484359478aa84683192292d23
Mohammad Juned Shaikh <143900727+shaikh-md-juned@users.noreply.github.com>
2023-11-17 22:52:28 +0530

    Add files via upload

---
+++ BTC/config.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * GR8 Faucet Script Lite
+ * https://gr8.cc
+ * 
+ * Copyright 2019-2021 GR8 Scripts, AvalonRychmon
+ * 
+ * GR8 Faucet Script Lite is free bare bones version of the GR8 Faucet Script.
+ * It was released so that anyone interested in operating a cryptocurrecy faucet
+ * would have an equal opportunity regardless of their financial position or
+ * personal knowledge of coding. 
+ *
+ * If you need assistance with this script, then please join us on Discord at
+ * https://GR8.cc/discord
+ * 
+ * I personally wish you great success on your journey! -AvalonRychmon
+ *
+ */
+
+## Database settings
+$host = 'sql111.epizy.com';
+$database = 'epiz_32873867_BTC';
+$username = 'epiz_32873867';
+$password = 'ZNFR9MQr9CFbr';
+
+## Faucet id, this should be changed if you have more than one installation.
+$faucetID = 'DIGI-BTC';
+
+## Show Error Messages - Set to false once installion is finished.
+$show_errors = false;
